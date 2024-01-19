ab34df51c55c3c1fc67aa8f146e3f2cf644c2af1
gityashh <yashrajput8359@gmail.com>
2024-01-20 01:22:06 +0530

    20th jan new

--- script.js
+++ script.js
@@ -4,3 +4,41 @@ gsap.from(".load h1",{
     stagger:.3,
     ease: 'power4',
 })
+gsap.from(".load span",{
+    y:140,
+    duration:.8,
+    delay:.6,
+    stagger:.3,
+    ease: 'power4',
+})
+gsap.from("#counter h3",{
+    opacity:0,
+    duration:2
+})
+gsap.from(".load h4",{
+    opacity:0,
+    duration:2
+})
+
+var grow = 0;
+
+var int = setInterval(function(){
+    if (grow<100) {
+        grow++;
+        document.querySelector("#counter h3").innerHTML = grow;
+    }
+},40)
+
+setTimeout(function(){
+    clearInterval(int);
+},40000)
+
+
+// gsap.to("#loader",{
+//     y:-1000,
+//     duration:.5,
+//     delay:4,
+//     ease:"power4.Out"
+// })
+
+var tl = gsap.timeline();
